dbca6f6734f4439eb345dbcc8c92bb3e6320ee1e
Alex M <amalki@piap.pl>
2020-08-18 13:24:04 +0200

    mtd: spi-nor-ids: adding ELNEC XT25F32B spi flash ID

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -78,6 +78,9 @@ const struct flash_info spi_nor_ids[] = {
 	{ INFO("at25sl321",	0x1f4216, 0, 64 * 1024,  64, SECT_4K) },
 	{ INFO("at26df081a", 	0x1f4501, 0, 64 * 1024,  16, SECT_4K) },
 #endif
+#ifdef CONFIG_SPI_FLASH_ELNEC /** ELNEC */
+	{ INFO("xt25f32b",  0x0b4016, 0, 4 * 1024,  1024, SECT_4K) },
+#endif /** ELNEC */
 #ifdef CONFIG_SPI_FLASH_EON		/* EON */
 	/* EON -- en25xxx */
 	{ INFO("en25q32b",   0x1c3016, 0, 64 * 1024,   64, 0) },
